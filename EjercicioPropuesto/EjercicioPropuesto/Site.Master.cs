dc2de8ffe7a98d0e51b00ba19907d0929dcf335c
Sebastian Carrera <73044857@ucsm.edu.pe>
2021-09-15 11:04:45 -0500

    Ejercicio propuesto

---
+++ EjercicioPropuesto/EjercicioPropuesto/Site.Master.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EjercicioPropuesto
+{
+    public partial class SiteMaster : MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Grafico.Visible = false;
+        }
+
+        protected void Mostrar_ServerClick(Object sender, EventArgs e)
+        {
+            ListItem elemento = lista.Items[lista.SelectedIndex];
+            if (elemento.Value == "Microonda")
+            {
+                Resultado.Style["color"] = "Purple";
+                Resultado.InnerText = "Codigo: " + "456268778 " + 
+                    "Nombre: " + "Microonda " + 
+                    "Descripcion: " + "Aparato de ondas electromagnéticas que permite cocinar y calentar alimentos en pocos minutos. " + 
+                    "Marca: " + "Samsung " + 
+                    "Precio: " + "S/.769.90 ";
+            }
+            if (elemento.Value == "Refrigerador")
+            {
+                Resultado.Style["color"] = "Purple";
+                Resultado.InnerText = "Codigo: " + "851647489 " + 
+                    "Nombre: " + "Refrigerador " + 
+                    "Descripcion: " + "Es un dispositivo de cocina para conservar la comida. " + 
+                    "Marca: " + "LG " + 
+                    "Precio: " + "S/.2800.99 ";
+
+            }
+            if (elemento.Value == "Tostadora")
+            {
+                Resultado.Style["color"] = "Purple";
+                Resultado.InnerText = "Codigo: " + "458963255 " + 
+                    "Nombre: " + "Tostadora " + 
+                    "Descripcion: " + "Es un pequeño aparato que sirve para tostar rebanadas de pan. " +  
+                    "Marca: " + "Holstein " + 
+                    "Precio: " + "S/.330.90 ";
+
+            }
+            if (elemento.Value == "Campana extractora")
+            {
+                Resultado.Style["color"] = "Purple";
+                Resultado.InnerText = "Codigo: " + "325784966 " + 
+                    "Nombre: " + "Campana extractora " + 
+                    "Descripcion: " + "Aparato de cocina que permite eliminar los malos olores, la grasa y la humedad. " + 
+                    "Marca: " + "Sole " + 
+                    "Precio: " + "S/.560.00 ";
+
+            }
+            if (elemento.Value == "Aspiradora")
+            {
+                Resultado.Style["color"] = "Purple";
+                Resultado.InnerText = "Codigo: " + "558931497 " + 
+                    "Nombre: " + "Aspiradora " + 
+                    "Descripcion: " + "Es un dispositivo que utiliza una bomba de aire para aspirar el polvo y otras partículas pequeñas de suciedad y que permite dejar así los pisos y alfombras impecables. " + 
+                    "Marca: " + "Electrolux " + 
+                    "Precio: " + "S/.249.00 ";
+
+            }
+        }
+
+        protected void MostrarGrafico_ServerClick(Object sender, EventArgs e)
+        {
+            Grafico.Src = "figura" + lista.SelectedIndex.ToString() + ".jpg";
+            Grafico.Alt = "Grafico Electrodomestico";
+            Grafico.Visible = true;
+        }
+
+
+    }
+}
---
+++ EjercicioPropuesto/EjercicioPropuesto/Site.Master.designer.cs
@@ -0,0 +1,125 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace EjercicioPropuesto
+{
+
+
+    public partial class SiteMaster
+    {
+
+        /// <summary>
+        /// Control cuerpo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl cuerpo;
+
+        /// <summary>
+        /// Control WebForm.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm WebForm;
+
+        /// <summary>
+        /// Control lista.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlSelect lista;
+
+        /// <summary>
+        /// Control boton.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputButton boton;
+
+        /// <summary>
+        /// Control etiqueta.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl etiqueta;
+
+        /// <summary>
+        /// Control Resultado.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl Resultado;
+
+        /// <summary>
+        /// Control Lista4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlSelect Lista4;
+
+        /// <summary>
+        /// Control Button1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputButton Button1;
+
+        /// <summary>
+        /// Control Button5.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button5;
+
+        /// <summary>
+        /// Control etiqueta3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label etiqueta3;
+
+        /// <summary>
+        /// Control MostrarGrafico.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputSubmit MostrarGrafico;
+
+        /// <summary>
+        /// Control Grafico.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlImage Grafico;
+    }
+}
